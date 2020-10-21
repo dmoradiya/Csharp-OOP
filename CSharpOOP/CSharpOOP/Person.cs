3e00ed707ae57cf83a7298e921733ac32cd6edb6
dhmec <dharmoradiya@gmail.com>
2020-10-21 10:30:01 -0600

    Instantiation and add more comments

--- CSharpOOP/CSharpOOP/Person.cs
+++ CSharpOOP/CSharpOOP/Person.cs
@@ -6,7 +6,31 @@ namespace CSharpOOP
 {
     class Person
     {
-      
-        
+
+        // Public items can be seen anywhere.
+        // Auto-Implemented Property (Automatically generates a default "getter" (accessor) and "setter" (mutator).
+        // AccessModifier DataType PropertyName { get; set; }
+        public string FirstName { get; set; }
+
+        // Private items cannot be seen outside of the class. Marking it as private enforces that all data to be stored MUST go through the property.
+        private string _lastName;
+        // Fully-Implemented Property (You specify the beahviour for the "getter" (accessor) and "setter" (mutator).
+        public string LastName
+        {
+            get
+            {
+                // For the getter, typically you will want to return the value from the backing variable.
+                // You may format it first, etc. but that is not necessary.
+                return _lastName;
+            }
+            set
+            {
+                // For the setter, typically you will want to assign the backing variable to the keyword "value" which represents what is being assigned to the property.
+                // You may perform validation here as well.
+                _lastName = value;
+            }
+        }
+
+
     }
 }
--- CSharpOOP/CSharpOOP/Program.cs
+++ CSharpOOP/CSharpOOP/Program.cs
@@ -6,7 +6,18 @@ namespace CSharpOOP
     {
         static void Main(string[] args)
         {
-           
+            // Instantiate the object using the "new" keyword - generate an instance of a class / blueprint.
+            Person person = new Person();
+            // The new object has all of the properties that are specified in the class, but they are tied to that object.
+            // One object's properties cannot see another object's properties without explicitly passing the values.
+
+            Console.Write("Please enter your first name: ");
+            person.FirstName = Console.ReadLine().Trim();
+
+            Console.Write("Please enter your last name: ");
+            person.LastName = Console.ReadLine().Trim();
+
+            Console.WriteLine($"Hello, {person.FirstName} {person.LastName}!");
         }
     }
 }
